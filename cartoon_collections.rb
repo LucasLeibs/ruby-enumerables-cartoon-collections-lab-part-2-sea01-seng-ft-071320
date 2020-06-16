bce88bd6616d3399afce3339284e144ddfba2bcd
Lucas Leiberman <dr.lucasleibs@gmail.com>
2020-06-16 19:31:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
 def square_array(array)
-  # Use an Enumerable to square every element in the passed in array
+  array.map do |element|
+    new_array << element * element 
+    end
+    # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
 end
 
